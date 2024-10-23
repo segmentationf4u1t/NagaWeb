a10aab7f9e6248a041b27208a8f3d0234d61510d
Segfault <cryptexunion@gmail.com>
2024-10-23 17:14:07 +0200

    types

---
+++ types/types.ts
@@ -0,0 +1,41 @@
+// types.ts
+export interface FeatureItem {
+	src: string;
+	alt: string;
+	title: string;
+	description: string;
+	id: string;
+}
+
+export interface Categories {
+	reasoning: string[];
+	coding: string[];
+	mathematics: string[];
+	data_analysis: string[];
+	language: string[];
+	if: string[];
+}
+
+export interface CheckedCategories {
+	[key: string]: {
+		average: boolean;
+		allSubcategories: boolean;
+	};
+}
+
+export interface ProcessedResults {
+	model: string;
+	global_average: number;
+	reasoning_average: number;
+	coding_average: number;
+	mathematics_average: number;
+	data_analysis_average: number;
+	language_average: number;
+	if_average: number;
+	[key: string]: string | number;
+}
+
+export interface SortConfig {
+	key: keyof ProcessedResults;
+	direction: "asc" | "desc";
+}
